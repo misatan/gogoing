183179edaf58dde864d2127aa7ed9eae2c2657b1
shuwen <2380510662@qq.com>
2023-04-26 08:38:29 +0800

    struct-01

---
+++ base/struct/demo01.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+//类型定义
+type newInt int
+
+//类型别名
+type myInt = int
+
+func main() {
+	var a newInt
+	var b myInt
+
+	fmt.Printf("type of a:%T\n", a) // tyep of a:main.newInt
+	fmt.Printf("type of b:%T\n", b) // type of b:int
+}
